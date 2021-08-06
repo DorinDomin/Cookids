6d65829f5b746fcd71902c5367a7d1b806aebfe6
shiraTurgeman <58763845+shiraTurgeman@users.noreply.github.com>
2021-08-06 14:41:25 +0300

    Delete resources directory

--- resources/users.py
+++
@@ -1,9 +0,0 @@
-from flask_restful import Resource
-from flask import request
-import random
-import string
-
-
-class User(Resource):
-    def get(self):
-        return {"message": "Hello, World!"}
